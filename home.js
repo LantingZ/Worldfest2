f1826bc7fe276d53c1ffdd963a012bff2ec41332
Lanting Zhu <86578501+LantingZ@users.noreply.github.com>
2023-01-27 21:28:59 +0000

    Added a globe

--- home.js
+++ home.js
@@ -3,29 +3,14 @@
   globe.loadPlugin(autorotate(10));
   globe.loadPlugin(planetaryjs.plugins.earth({
     topojson: { file:   '/world.json' },
-    oceans:   { fill:   '#2278B0' },
-    land:     { fill:   '#17903F' },
-    borders:  { stroke: '#17903F' }
+    oceans:   { fill:   '#4A9FE8' },
+    land:     { fill:   '22B03E' },
+    borders:  { stroke: '#2E2E2E' }
   }));
   globe.loadPlugin(lakes({
     fill: '#4A9FE8'
   }));
-  globe.loadPlugin(planetaryjs.plugins.pings());
-  globe.loadPlugin(planetaryjs.plugins.drag({
-    onDragStart: function() {
-      this.plugins.autorotate.pause();
-    },
-    onDragEnd: function() {
-      this.plugins.autorotate.resume();
-    }
-  }));
   globe.projection.scale(175).translate([175, 175]).rotate([0, -10, 0]);
-
-  setInterval(function() {
-    var lat = Math.random() * 170 - 85;
-    var lng = Math.random() * 360 - 180;
-    globe.plugins.pings.add(lng, lat, { color: 'white', ttl: 2000, angle: Math.random() * 10 });
-  }, 150);
 
   var canvas = document.getElementById('rotatingGlobe');
   if (window.devicePixelRatio == 2) {
@@ -74,51 +59,10 @@
         planet.withSavedContext(function(context) {
           context.beginPath();
           planet.path.context(context)(lakes);
-          context.fillStyle = options.fill || 'black';
+          context.fillStyle = options.fill || 'blue';
           context.fill();
         });
       });
     };
   };
 })();
-
-var counterContainer = document.querySelector(".counter");
-var visitCount = localStorage.getItem("page_view");
-
-if (visitCount) {
-  visitCount = Number(visitCount) + 1;
-  localStorage.setItem("page_view", visitCount);
-} else {
-  visitCount = 1;
-  localStorage.setItem("page_view", 1);
-}
-counterContainer.innerHTML = visitCount;
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
